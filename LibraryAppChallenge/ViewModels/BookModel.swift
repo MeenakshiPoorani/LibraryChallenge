073ad8ae77b5583d6e3004408c2d7498e5198c2e
MeenakshiPoorani <smeenakshipoorani@gmail.com>
2022-03-04 17:39:30 +0530

    - Add clearer comments

--- LibraryAppChallenge/ViewModels/BookModel.swift
+++ LibraryAppChallenge/ViewModels/BookModel.swift
@@ -17,7 +17,12 @@ class BookModel : ObservableObject {
     
     func updateFavourite(forId: Int) {
         
-        /* The following will not work because index and ids are different parameters. ex. lets say we want to toggle book[0].isFavourite, so forId passed into the function call for book[0] is forID = 1 (book[0] has id = 1). In case of first round, index would have value 0 and forId would have value 1 and is skipped. In next iteration, index is 1 and forId is 1 and you toggle book[index].isFavourite,since index = 1, you're toggling the next book actually i.e. book[1] instead of desired book[0]
+        /* The following will not work because index and ids are different parameters.
+         
+         ex. lets say we want to toggle book[0].isFavourite, so forId passed into the function call for book[0] is forID = 1 (book[0] has id = 1).
+             In case of first round, index would have value 0 and forId would have value 1 and is skipped.
+             In next iteration, index is 1 and forId is 1 and you toggle book[index].isFavourite,since index = 1, you're toggling the next book actually i.e. book[1] instead of desired book[0]
+         
         for index in 0...books.count-1
         {
             if index == forId {
@@ -27,7 +32,8 @@ class BookModel : ObservableObject {
         }
         */
         
-        /*if let index = books.firstIndex(where: { $0.id == forId }) {
+        /* Chris's solution :
+         if let index = books.firstIndex(where: { $0.id == forId }) {
             books[index].isFavourite.toggle()
         }*/
         
